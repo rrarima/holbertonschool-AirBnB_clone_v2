63b8288422341c37e8fc0063cd71cbed4c3262b9
rrarima <5708@holbertonstudents.com>
2023-04-03 12:38:22 +1000

    added hbnb dir

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that start Flask web app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route ():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
