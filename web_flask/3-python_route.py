c1c241023039163a5aadf16051be1bc37809ef8d
rrarima <5708@holbertonstudents.com>
2023-04-03 16:38:34 +1000

    added python dunamic route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that start Flask web app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_cool(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/<path:subpath>", strict_slashes=False)
+@app.route("/<path:subpath>/<text>", strict_slashes=False)
+def python_route(subpath,text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
