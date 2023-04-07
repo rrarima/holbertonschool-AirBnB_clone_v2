0c22021d03b58da206b0a785b83ca545fb40334c
rrarima <5708@holbertonstudents.com>
2023-04-07 16:24:01 +1000

    pycodestyle

--- models/state.py
+++ models/state.py
@@ -4,13 +4,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """ State class """
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state', cascade='all, delete')
-
 
     @property
     def cities(self):
