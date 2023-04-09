0059d631e83eef0ceb8ab53ed1c45865173318da
rrarima <5708@holbertonstudents.com>
2023-04-09 19:51:40 +1000

    retrieve data from sql database

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that start Flask web app"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list(n):
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+@app.teardown_appcontext
+def teardown():
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
